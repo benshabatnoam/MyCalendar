9d41d49c02fc8d6c8cfb2ac8bc5399a6645a1057
Noam Ben Shabat <noamb@Taldor.corp>
2017-05-18 14:00:34 +0300

    one call to web api to get all page permissions at once. store the permissions in the permissions service, and return isPagePermitted from that stored permissions array.

--- src/app/modules/taldor/taldor-permissions-guard.service.ts
+++ src/app/modules/taldor/taldor-permissions-guard.service.ts
@@ -15,18 +15,27 @@ export class TaldorPermissionsGuard implements CanActivate {
     constructor(private permissionsService: TaldorPermissionsService,
     private router: Router) { }
 
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-        return this.permissionsService.isPagePermitted(state.url)
-            .map(response => {
-                if (response)
-                    return true;
-                else {
-                    this.router.navigateByUrl('notPermitted');
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean{
+        var response = this.permissionsService.isPagePermitted(state.url);
+        if (typeof response === 'boolean')
+            return this.handleResponse(response);
+        else {
+            return (response as Observable<boolean>)
+                .map(response => {
+                    return this.handleResponse(response);
+                })
+                .catch((error): any => {
                     return false;
-                }
-            })
-            .catch((error): any => {
-                return false;
-            });;
+                });;
+        }
+    }
+
+    handleResponse(response: boolean): boolean {
+        if (response)
+            return true;
+        else {
+            this.router.navigateByUrl('notPermitted');
+            return false;
+        }
     }
 }
--- src/app/modules/taldor/taldor-permissions.service.ts
+++ src/app/modules/taldor/taldor-permissions.service.ts
@@ -7,30 +7,32 @@ import 'rxjs/add/Operator/catch';
 @Injectable()
 export class TaldorPermissionsService {
     serviceUrl: string = 'http://localhost//Permissions/PermissionsServices/PermissionsServices/api/Permissions/';
+    permissions: string[];
 
     constructor(private http: Http) { }
 
-    isPagePermitted(url: string): Observable<boolean> {
-        return this.http
-            .post(
-                this.serviceUrl + 'IsPagePermitted?url=' + url,
-                null,
-                { withCredentials: true}
-            )
-            .map((response) => {
-                return response.text().toString() == 'true';
-            })
-            .catch((error): any => {
-                console.error('error in isPermitted. error: ' + error)
-            });
+    isPagePermitted(url: string): Observable<boolean> | boolean {
+        if (!this.permissions) {
+            return this.http.get(this.serviceUrl + 'GetPagePermissions')
+                .map((response) => {
+                    this.permissions = response.json().split(';');
+                    return this.permissions.includes(url);
+                })
+                .catch((error): any => {
+                    console.error('error in isPermitted. error: ' + error)
+                    return false;
+                });
+        }
+        else {
+            return this.permissions.includes(url);
+        }
     }
 
     isComponentPermitted(component: string): Observable<boolean> {
         return this.http
             .post(
                 this.serviceUrl + 'IsComponentPermitted?component=' + component,
-                null,
-                { withCredentials: true}
+                null
             )
             .map((response) => {
                 return response.text().toString() == 'true';
